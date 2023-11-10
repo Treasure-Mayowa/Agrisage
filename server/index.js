8ea4dccb939a4e4672b712cc61698e00518c2d5d
Treasure Mayowa <treasuremayowa@outlook.com>
2023-11-10 20:28:38 +0100

    Add message response api

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 require("dotenv").config()
 const bodyParser = require('body-parser')
@@ -12,12 +13,19 @@ const { MessagingResponse } = require('twilio').twiml;
 const port = 3001
 
 app.listen(port, () => {
-    console.log(`server started on port ${port}`);
+    console.log(`server started on port ${port}`)
 })
 
+app.use(express.static(path.join(__dirname, '..', 'build')))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(pino)
+
+app.get('', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+
 
 app.post('/api/messages', (req, res) => {
     console.log('Received data:', req.body)
@@ -46,12 +54,30 @@ app.post('/api/messages', (req, res) => {
   });
   
 app.post('/api/sms', (req, res) => {
-  const twiml = new MessagingResponse();
+  const twiml = new MessagingResponse()
 
-  twiml.message('Sustainble agriculture is the new cool!');
+  var user_message = req.body.Body
 
+  var raw = `{\n    \"temperature\": 0.6,\n    \"messages\": [\n      {\n        \"role\": \"system\",\n        \"content\": \"You are a bot designed to educate farmers on sustainable agriculture\"\n      },\n      {\n        \"role\": \"user\",\n        \"content\": \"${user_message}\"\n      }\n    ],\n    \"model\": \"llama-2-chat-70b-4k\",\n    \"stream\": false,\n    \"max_tokens\": 1000\n  }`
+
+  var requestOptions = {
+    method: 'POST',
+    body: raw,
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": process.env.API_KEY
+    },
+    redirect: 'follow'
+  };
+
+  fetch("https://chat.nbox.ai/api/chat/completions", requestOptions)
+    .then(response => response.json())
+    .then(result => 
+      message = result.choices[0].message.content,
+      twiml.message(message))
+    .catch(error => console.log('error', error))
+    
   res.type('text/xml').send(twiml.toString())    
-
 })
 
 app.get('/api/greeting', (req, res) => {
